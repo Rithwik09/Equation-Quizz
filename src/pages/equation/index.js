e548fb0ea030eed0d4f79f0c56ca6201290ec757
rithwik09 <rithwik.chaithu.09@gmail.com>
2024-06-21 20:04:51 +0530

    added completion

--- src/pages/equation/index.js
+++ src/pages/equation/index.js
@@ -5,9 +5,11 @@ import 'tailwindcss/tailwind.css';
 const Users = () => {
   const isMounted = useRef(false);
   const [data, setData] = useState('');
-
+  const [answer, setAnswer] = useState('');
+  const [answerError, setAnswerError] = useState('');
+  const [answerResponse, setAnswerResponse] = useState('');
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-  
+
   useEffect(() => {
     if (!isMounted.current) {
       fetchSums();
@@ -17,39 +19,89 @@ const Users = () => {
 
   const fetchSums = async () => {
     try {
-      const response = await axios.post(`${baseUrl}getequation`);
-      setData(response.data.equation); 
-      console.log(response.data.equation);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        }
+      const response = await axios.get(`${baseUrl}getequation`);
+      setData(response.data.equation);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   };
-  const handleClick = () => {
-   
+
+  const handleInputChange = (e) => {
+    setAnswer(e.target.value);
+    if (e.target.value.trim() !== '') {
+      setAnswerError('');
+    }
   };
-  return (              
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    if (!answer.trim()) {
+      setAnswerError('Enter Answer');
+      return;
+    }
+    try {
+      const response = await axios.post(`${baseUrl}checkequation`, {
+        equation: data,
+        answer: answer
+      });
+      if (response.data.result === 'Correct') {
+        setAnswerResponse('Correct');
+        setAnswerError(''); 
+        fetchSums();
+        setAnswer('');
+        setTimeout(() => {
+          setAnswerResponse('');
+        }, 1500);
+      } else if (response.data.result === 'Incorrect') {
+        setAnswerError('Incorrect');
+        setAnswerResponse(''); 
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const handleNext = () => {
+    setAnswerError('');
+    setAnswerResponse('');
+    fetchSums();
+  };
+  
+
+  return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="bg-blue-100 p-6 rounded-lg shadow-lg mt-2">
         <div className="border-b mb-1">
           <h2 className="text-lg font-bold">Equation</h2>
         </div>
-          {data}
+        <p>{data}</p>
         <div className="grid gap-2 justify-items-start">
-          <input className="rounded border-solid border-2 border-indigo-600" type="text"/>
-          <div>
-          <button 
-          type="button" 
-          class="text-white bg-blue-700 hover:bg-blue-800 rounded-lg text-sm px-2 py-1.5 dark:bg-blue-600 dark:hover:bg-blue-700"
-          onClick={handleClick} 
-          >
-          Submit
-        </button>
+          <input
+            className="rounded border-solid border-2 border-indigo-600"
+            type="text"
+            value={answer}
+            onChange={handleInputChange}
+          />
+          {answerError && <p className="text-red-500">{answerError}</p>}
+          {answerResponse && <p className="text-green-500">{answerResponse}</p>}
+          <div className="flex gap-3 justify-items-start">
+            <button
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 rounded-lg text-sm px-2 py-1.5 dark:bg-blue-600 dark:hover:bg-blue-700"
+              onClick={handleClick}
+            >
+              Submit
+            </button>
+            <button
+            className="text-white bg-green-700 hover:bg-green-800 rounded-lg text-sm px-2 py-1.5 dark:bg-green-600 dark:hover:bg-green-700"
+            onClick={handleNext}
+            >
+              Next</button>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Users;
-
